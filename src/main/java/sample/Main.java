f543d8f5ceb0f472fa70f98394fa6c0ee0e1bdaa
shalevf6 <38422152+shalevf6@users.noreply.github.com>
2018-10-27 16:05:14 +0300

    end of sqlite database example

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,9 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import java.sql.*;
 
 public class Main extends Application {
 
@@ -17,29 +16,339 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        connect();
+        //connect();
+        //createNewDatabase("test.db");
+        String newTable1 = "CREATE TABLE IF NOT EXISTS materials (\n" +
+                "id integer PRIMARY KEY,\n" +
+                "description text NOT NULL);";
+        String newTable2 = "CREATE TABLE IF NOT EXISTS inventory (\n" +
+        "warehouse_id integer,\n" +
+        "material_id integer,\n" +
+        "qty real,\n" +
+        "PRIMARY KEY (warehouse_id, material_id),\n" +
+        "FOREIGN KEY (warehouse_id) REFERENCES warehouses (id),\n" +
+                "FOREIGN KEY (material_id) REFERENCES materials (id));";
+
+        createNewTable(newTable1);
+        createNewTable(newTable2);
+
+        createNewTable("CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");");
+
+        //createNewTable();
+        Main app = new Main();
+        // insert three new rows
+        //app.insert("Raw Materials", 3000);
+        //app.insert("Semifinished Goods", 4000);
+        //app.insert("Finished Goods", 5000);
+
+        /*
+        app.update(3, "Finished Products", 5000);
+        app.selectAll();
+        System.out.println();
+        app.getCapacityGreaterThan(3600);
+        System.out.println();
+        app.update(3, "Finished Products", 5500);
+        app.selectAll();
+        System.out.println();
+        app.delete(3);
+        app.selectAll();
+        */
+
+        app.insert("Raw Materials", 3000);
+        app.insert("Semifinished Goods", 4000);
+        app.insert("Finished Goods", 5000);
+
+
+        app.addInventory("HP Laptop", 3, 100);
+
+       app.selectAll("warehouses");
+       System.out.println();
+       app.selectAll("inventory");
+       System.out.println();
+       app.selectAll("materials");
+
     }
 
-    private static void connect() {
+    /**
+     * Create a new material and add initial quantity to the warehouse
+     *
+     * @param material
+     * @param warehouseId
+     * @param qty
+     */
+    public void addInventory(String material, int warehouseId, double qty) {
+        // SQL for creating a new material
+        String sqlMaterial = "INSERT INTO materials(description) VALUES(?)";
+
+        // SQL for posting inventory
+        String sqlInventory = "INSERT INTO inventory(warehouse_id,material_id,qty)"
+                + "VALUES(?,?,?)";
+
+        ResultSet rs = null;
         Connection conn = null;
+        PreparedStatement pstmt1 = null, pstmt2 = null;
+
         try {
-            // db parameters
-            String url = "jdbc:sqlite:sqlite_database_file_path";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
+            // connect to the database
+            conn = this.connect();
+            if(conn == null)
+                return;
 
-            System.out.println("Connection to SQLite has been established.");
+            // set auto-commit mode to false
+            conn.setAutoCommit(false);
 
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            // 1. insert a new material
+            pstmt1 = conn.prepareStatement(sqlMaterial,
+                    Statement.RETURN_GENERATED_KEYS);
+
+            pstmt1.setString(1, material);
+            int rowAffected = pstmt1.executeUpdate();
+
+            // get the material id
+            rs = pstmt1.getGeneratedKeys();
+            int materialId = 0;
+            if (rs.next()) {
+                materialId = rs.getInt(1);
+            }
+
+            if (rowAffected != 1) {
+                conn.rollback();
+            }
+            // 2. insert the inventory
+            pstmt2 = conn.prepareStatement(sqlInventory);
+            pstmt2.setInt(1, warehouseId);
+            pstmt2.setInt(2, materialId);
+            pstmt2.setDouble(3, qty);
+            //
+            pstmt2.executeUpdate();
+            // commit work
+            conn.commit();
+
+        } catch (SQLException e1) {
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+            } catch (SQLException e2) {
+                System.out.println(e2.getMessage());
+            }
+            System.out.println(e1.getMessage());
         } finally {
             try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (pstmt1 != null) {
+                    pstmt1.close();
+                }
+                if (pstmt2 != null) {
+                    pstmt2.close();
+                }
                 if (conn != null) {
                     conn.close();
                 }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
+            } catch (SQLException e3) {
+                System.out.println(e3.getMessage());
             }
+        }
+    }
+
+    /**
+     * Delete a warehouse specified by the id
+     *
+     * @param id
+     */
+    public void delete(int id) {
+        String sql = "DELETE FROM warehouses WHERE id = ?";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // set the corresponding param
+            pstmt.setInt(1, id);
+            // execute the delete statement
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Update data of a warehouse specified by the id
+     *
+     * @param id
+     * @param name name of the warehouse
+     * @param capacity capacity of the warehouse
+     */
+    public void update(int id, String name, double capacity) {
+        String sql = "UPDATE warehouses SET name = ? , "
+                + "capacity = ? "
+                + "WHERE id = ?";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // set the corresponding param
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.setInt(3, id);
+            // update
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Get the warehouse whose capacity greater than a specified capacity
+     * @param capacity
+     */
+    public void getCapacityGreaterThan(double capacity){
+        String sql = "SELECT id, name, capacity "
+                + "FROM warehouses WHERE capacity > ?";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt  = conn.prepareStatement(sql)){
+
+            // set the value
+            pstmt.setDouble(1,capacity);
+            //
+            ResultSet rs  = pstmt.executeQuery();
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * select all rows in the warehouses table
+     */
+    public void selectAll(String table){
+        String sql = null;
+
+        if (table.equals("warehouses"))
+            sql = "SELECT id, name, capacity FROM " + table;
+
+        if (table.equals("inventory"))
+            sql = "SELECT warehouse_id, material_id, qty FROM " + table;
+
+        if (table.equals("materials)"))
+            sql = "SELECT id, description FROM " + table;
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            if (table.equals("warehouses")) {
+                while (rs.next()) {
+                    System.out.println(rs.getInt("id") + "\t" +
+                            rs.getString("name") + "\t" +
+                            rs.getDouble("capacity"));
+                }
+            }
+
+            if (table.equals("inventory")) {
+                while (rs.next()) {
+                    System.out.println(rs.getInt("warehouse_id") + "\t" +
+                            rs.getInt("material_id") + "\t" +
+                            rs.getInt("qty"));
+                }
+            }
+
+            if (table.equals("materials)")) {
+                while (rs.next()) {
+                    System.out.println(rs.getInt("id") + "\t" +
+                            rs.getString("description"));
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Connect to the test.db database
+     *
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:sqlite_database_file_path";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    /**
+     * Insert a new row into the warehouses table
+     *
+     * @param name
+     * @param capacity
+     */
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO warehouses(name,capacity) VALUES(?,?)";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Create a new table in the test database
+     *
+     */
+    public static void createNewTable(String sql) {
+        // SQLite connection string
+        String url = "jdbc:sqlite:sqlite_database_file_path";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Connect to a sample database
+     *
+     * @param fileName the database file name
+     */
+    public static void createNewDatabase(String fileName) {
+
+        String url = "jdbc:sqlite:sqlite_database_file_path" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 
