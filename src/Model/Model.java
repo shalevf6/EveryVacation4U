c4e742d7de01fb5f88f30e895aacdd6f999a0365
omerrauchbach <44476889+omerrauchbach@users.noreply.github.com>
2018-10-29 09:21:12 +0200

    Revert "function create in model class"

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,8 +1,6 @@
 package Model;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 public class Model {
 
@@ -13,25 +11,7 @@ public class Model {
     }
 
     public boolean create(String userName, String password, String birthDate, String FirstName, String LastName, String city){
-        String sql = "INSERT INTO warehouses(userName, password, birthDate,FirstName, LastName, city)) VALUES(?,?,?,?,?,?)";
-
-        if (this.connection != null) {
-            try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-                pstmt.setString(1, userName);
-                pstmt.setString(2, password);
-                pstmt.setString(3, birthDate);
-                pstmt.setString(4, FirstName);
-                pstmt.setString(5, LastName);
-                pstmt.setString(6, city);
-
-                pstmt.executeUpdate();
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-            return false;
-        }
-
-        return true;
+        return false;
     }
 
     public boolean update(String fieldToChange, String newInput) {
